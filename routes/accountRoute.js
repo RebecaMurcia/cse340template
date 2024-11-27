3e1b6d78de7f247b1525bcc54e9fff40f8f37912
Rebeca Murcia <131334317+RebecaMurcia@users.noreply.github.com>
2024-11-26 23:35:27 -0800

    w04

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,4 +7,11 @@ const utilities = require("../utilities")
   //Deliver login view
   router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
+   //Deliver registration view
+   router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+   //POST router registration view
+   router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+
 module.exports = router;
